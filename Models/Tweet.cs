2ad18d2d5ce5272dc4e3c77d07dadcb9d62d8d4d
paulverger <paulverger@gmail.com>
2020-12-27 07:57:50 -0800

    Added a view and controller for Scheduler

---
+++ Models/Tweet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaScheduler.Models
+{
+	public class Tweet
+	{
+		public string TweetText { get; set; }
+	}
+}
