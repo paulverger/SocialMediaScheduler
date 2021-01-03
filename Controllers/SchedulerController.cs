cc0ffdcc1508ec95fde102dd44a09b3a42eb7426
paulverger <paulverger@gmail.com>
2021-01-03 07:36:08 -0800

    Put secrets in secrets manager.

--- Controllers/SchedulerController.cs
+++ Controllers/SchedulerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SocialMediaScheduler.Models;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,23 @@ namespace SocialMediaScheduler.Controllers
 {
 	public class SchedulerController : Controller
 	{
-		private const string TwitterApiKey = "oL3nZP2goQaTQzKn10T01TrIE";
-		private const string TwitterApiSecret = "azrebsfFEec2nnVVwESSGkJbMAl51NuJvRobtvY3Qk3TZ9En0O";
-		private const string AccessToken = "1343720920498597890-ROtI9CYGcHgOnziHrkVA4rs0OdBAiT";
-		private const string AccessTokenSecret = "SjF1vYkZ3h7GY2nNDmI2pM5myUJKl5QzquFkzWTYCY9tr";
+		private string _TwitterApiKey;
+		private string _TwitterApiSecret;
+		private string _AccessToken;
+		private string _AccessTokenSecret;
+
+		private readonly IConfiguration _config;
+
+		public SchedulerController(IConfiguration config)
+		{
+		    _config = config;
+			_TwitterApiKey = _config["Twitter:ApiKey"];
+			_TwitterApiSecret = _config["Twitter:ApiKeySecret"];
+			_AccessToken = _config["Twitter:AccessToken"];
+			_AccessTokenSecret = _config["Twitter:AccessTokenSecret"];
+			
+		}
+
 
 		[HttpGet]
 		public IActionResult Index()
@@ -23,7 +37,7 @@ namespace SocialMediaScheduler.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Index(Tweet tweet)
 		{
-			var userClient = new TwitterClient(TwitterApiKey, TwitterApiSecret, AccessToken, AccessTokenSecret);
+			var userClient = new TwitterClient(_TwitterApiKey, _TwitterApiSecret, _AccessToken, _AccessTokenSecret);
 			var user = await userClient.Users.GetAuthenticatedUserAsync();
 			var userTweet = await userClient.Tweets.PublishTweetAsync(tweet.TweetText.ToString());
 			return View();
